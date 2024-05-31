ba73d7a626d7747cf161cc9f01e1863c95f97473
Dr.Angelicus <Kittyhellokitty555@gmail.com>
2024-05-31 09:02:59 +0300

    Добавлены навигационные ссылки

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,13 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+                    {{ __("Вы вошли в систему.") }}<br>
+                    <hr>
+                    <ul>
+                        <li><a href="/books">Вернуться на страницу "Список книг"</a></li>
+                        <li><a href="/authors">Вернуться на страницу "Список авторов"</a></li>
+                    </ul>
+
                 </div>
             </div>
         </div>
--- resources/views/incs/navbar.blade.php
+++ resources/views/incs/navbar.blade.php
@@ -1,5 +1,11 @@
 <nav>
     <ul>
+        @if(\Illuminate\Support\Facades\Auth::check())
+            <li><a href="/profile">Профиль пользователя</a></li>
+        @else
+            <li><a href="/login">Залогиниться</a></li>
+            <li><a href="/register">Зарегистрироваться</a></li>
+        @endif
         <li><a href="/books">Список книг</a></li>
         <li><a href="/authors">Список авторов</a></li>
     </ul>
